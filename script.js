15583ccb297cc8d870004f236a8b1bd6f8d27ec2
Allison Tarr <hello@allisontarr.com>
2015-04-07 00:21:59 -0400

    adjusting endpoint

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ guac.clientSecret = 'YKEJGAP4EJ12E4QR1ZZ3K5WR0W0AZSPDZCOEXKYX3TISEMRU';
 		e.preventDefault();
 		// Take info from input and store it in variable
 		guac.location = $('.location').val();
-		console.log('We should totally get some guacamole in ' + guac.location);
+		console.log('We should eat some guacamole in ' + guac.location);
 		// Run function with user entered location
 		guac.getData(guac.location);
+		// $('.location').val();
 
-		// $('.location').val('');
 	});
 } // end .init
 
@@ -30,11 +30,9 @@ guac.getData = function(place) {
 	$.ajax( {
 
 		// url : 'https://api.foursquare.com/v2/venues/explore?&v=20151010',
-
 		// url : 'https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&client_id=M01QAACEECGNTWIG3C3GEC5BEX3SNLI2EZ4SJ0WHVQEY3ZA5&client_secret=YKEJGAP4EJ12E4QR1ZZ3K5WR0W0AZSPDZCOEXKYX3TISEMRU&v=20151010',
 
 		url : 'https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&client_id=' + guac.clientID + '&client_secret=' + guac.clientSecret + '&v=20151010',
-
 		dataType : 'jsonp',
 		type : 'GET',
 		v : '20160806',
@@ -53,15 +51,22 @@ guac.getData = function(place) {
 			openNow : true,
 			venuePhotos : '1'
 		},
-		success : function(response) {
-			console.log(response);
-			console.log("Success function called" + response);
+		success : function(result) {
+			console.log(result);
+			console.log("The eagle has landed plus" + result);
 			// Display data that has come back using another function
 		}
 	}); // end ajax
 } // end .getData
 
 // Function that is used to display information in html
+
+guac.display = function(result) {
+
+	var guacResults = result.response.groups[0].items[0].venue.name;
+	console.log(guacResults);
+}
+
 
 // Document ready statement
 $(function() {
