b006e9d972ee29b591502cfcdc751fd805ab2240
Allison Tarr <hello@allisontarr.com>
2015-04-13 17:14:18 -0400

    Beginning of Loading Icon

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ guac.clientSecret = 'YKEJGAP4EJ12E4QR1ZZ3K5WR0W0AZSPDZCOEXKYX3TISEMRU';
 guac.init = function() {
 	//What happens when you submit the form?
 	$('.search').on('submit', function(e) {
+
+		$(".spinner").addClass('loading');
 		// Prevent the default
 		e.preventDefault();
 
@@ -16,12 +18,7 @@ guac.init = function() {
 				  guac.lat = position.coords.latitude;
 				  guac.lon = position.coords.longitude;
 				  console.log("I got it!" + guac.lon + " , " + guac.lat);
-		// Take info from input and store it in variable
-		// guac.location = $('.location').val();
-		// console.log('We should eat some guacamole in ' + guac.location);
-		// Run function with user entered location
-		// guac.getPlaces(guac.location);
-		// $('.location').val();
+		//run AJAX call function
 			guac.getPlaces();
 		}); 
 	}); //end submit
@@ -33,11 +30,11 @@ guac.init = function() {
 
 // Function that will go and get information from the API
 guac.getPlaces = function(place) {
+
 	// Ajax response to gather data from API
 	$.ajax( {
 		// url : 'https://api.foursquare.com/v2/venues/explore?&v=20151010',
 		// url : 'https://api.foursquare.com/v2/venues/explore?ll=40.7,-74&client_id=M01QAACEECGNTWIG3C3GEC5BEX3SNLI2EZ4SJ0WHVQEY3ZA5&client_secret=YKEJGAP4EJ12E4QR1ZZ3K5WR0W0AZSPDZCOEXKYX3TISEMRU&v=20151010',
-		// url : 'https://api.foursquare.com/v2/venues/explore?ll=43.6,-79.3&client_id=' + guac.clientID + '&client_secret=' + guac.clientSecret + '&v=20151010',
 		url : 'https://api.foursquare.com/v2/venues/explore?ll=' + guac.lat + ',' + guac.lon + '&client_id=' + guac.clientID + '&client_secret=' + guac.clientSecret + '&v=20151010',
 		dataType : 'jsonp',
 		type : 'GET',
@@ -49,51 +46,41 @@ guac.getPlaces = function(place) {
 			client_id : guac.clientId,
 			client_secret : guac.clientSecret,
 			ll : guac.lat + "," + guac.lon,
-			// near : guac.location,
-			radius : 1200,
+			radius : 2000,
 			limit : 9,
 			query : 'guacamole',
-			openNow : true,
+			// openNow : true,
 			venuePhotos : '1'
 		},
 		success : function(result) {
-			console.log(result);
-			console.log(guac.lat + "," + guac.lon);
 			guac.displayPlaces(result);	
+			$(".spinner").removeClass('loading');
 		}
 	}); // end ajax
 } // end .getPlaces
 
 // Function that is used to display information in html
-
 guac.displayPlaces = function(result) {
 
 	//clear old results to find new ones??
 	$(".results").html('');
-
 	//create variable that holds path to data
 	var places = result.response.groups[0].items;
 	console.log(places);
 
 	// LOOP that will go through array being held in items above
 	for (i=0; i < places.length; i++) {
-
 		//create blank div
 		var div = $('<div>').addClass('places');
-
 		//get venue name
 		var h3 = $('<h3>').text(places[i].venue.name);
-
 		//get venue address
 		var addrPrefix = "https://foursquare.com/v/" + places[i].venue.id;
 		var addr = $('<a class="addr">').attr('href', addrPrefix).html('<i class="fa fa-home"></i> ' + places[i].venue.location.address);
-
 		//get distance from current location
 		var dist = $('<p>').text('You are ' + places[i].venue.location.distance + 'm away.');
-
 		//get rating of venue
 		var rating = $('<p>').text(places[i].venue.rating);
-
 		//twitter
 		var twitterPrefix = "https://www.twitter.com/";
 		if(places[i].venue.contact.twitter) {
@@ -105,13 +92,10 @@ guac.displayPlaces = function(result) {
 		//venue photo
 		var photoPrefix = places[i].venue.photos.groups[0].items[0];
 		var photo = $('<img>').attr('src',photoPrefix.prefix + photoPrefix.height + photoPrefix.suffix);
-
 		//put all of the variables into the div
 		div.append(h3, photo, rating, dist, addr, tweet);
-
 		//append that div to the results section of the html
 		$('section.results').append(div);
-
 	}
 }
 
